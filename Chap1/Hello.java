fa76fbd623638039630fea3eddf89b9fe10b4357
ucsaeki <ddt.ys.saeki@gmail.com>
2024-04-03 17:21:00 +0900

    first commit

---
+++ Chap1/Hello.java
@@ -0,0 +1,12 @@
+package Chap1;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("初めてのJavaプログラム。");//画面への出力を行うプログラム。
+		System.out.println("画面に出力しています。");//画面への出力を行うプログラム。
+
+	}
+
+}
