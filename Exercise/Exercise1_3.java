0f8882fd8cc595cc9f9bf391b10573e4ffd9ae70
ucsaeki <ddt.ys.saeki@gmail.com>
2024-04-04 11:56:42 +0900

    演習1-1～1-3

---
+++ Exercise/Exercise1_3.java
@@ -0,0 +1,28 @@
+package Exercise;
+
+/* 
+ * クラス名  Exercise1_3
+ * 概要 名前を縦にし姓と名の間に空白を入れて表示するメソッド。
+ * 作成者 Y.Saeki
+ * 作成日 2024/04/04
+ */
+public class Exercise1_3 {
+	/* 
+	 * 関数名  main
+	 * 概要 名前を縦にし姓と名の間に空白を入れて表示するメソッド。
+	 * 引数 なし
+	 * 返り値 なし
+	 * 作成者 Y.Saeki
+	 * 作成日 2024/04/04
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		//佐伯を一文字ずつ縦に表示させるために\nを使用して改行。
+		System.out.println("佐\n伯");
+		//空白を表示するために表示なしで改行。
+		System.out.println();
+		//友詞を一文字ずつ縦に表示させるために\nを使用して改行。
+		System.out.println("友\n詞");
+	}
+
+}
