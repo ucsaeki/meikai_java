4f51400fbd41193425ba8ad257503b9da6074df5
ucsaeki <ddt.ys.saeki@gmail.com>
2024-07-25 14:03:34 +0900

    7/25

--- Exercise3/Exercise3_2.java
+++ Exercise3/Exercise3_2.java
@@ -1,6 +1,8 @@
 package Exercise3;
+
 //Scannerクラスをインポート。
 import java.util.Scanner;
+
 /* 
  * クラス名 Exercise3_2
  * 概要 後に入力した整数が先に入力した整数の約数であるかを判定するメソッド。
@@ -19,7 +21,7 @@ public class Exercise3_2 {
 	public static void main(String[] args) {
 		//Scannerクラスに変数を設定。
 		Scanner standerdInput = new Scanner(System.in);
-		
+
 		//整数値の入力を促す。
 		System.out.print("整数A：");
 		//入力を読み込む。
@@ -28,27 +30,28 @@ public class Exercise3_2 {
 		System.out.print("整数B：");
 		//入力を読み込む。
 		int secondVariable = standerdInput.nextInt();
-		
+
 		//約数が0の場合
-		if ( secondVariable == 0) {
+		if (secondVariable == 0) {
 			//割られる数が0の場合
-			if ( firstVariabale ==0 ) {
+			if (firstVariabale == 0) {
 				//0は0の約数であることを表示。
 				System.out.println("BはAの約数です。");
-			//そうでない場合。
+				//そうでない場合。
 			} else {
 				//0で割ることはできず、0は約数にならないことを表示。
 				System.out.println("0は約数になりません");
-			//A÷Bの余りが0の場合。
-			}} else if ( firstVariabale % secondVariable == 0 ) {
+				//A÷Bの余りが0の場合。
+			}
+		} else if (firstVariabale % secondVariable == 0) {
 			//BがAの約数であることを表示。
 			System.out.println("BはAの約数です。");
 			//そうではない場合
-			} else {
+		} else {
 			//文章を表示。
 			System.out.println("BはAの約数ではありません。");
-			}
-		
+		}
+
 	}
 
 }
--- Exercise4/Exercise4_15.java
+++ Exercise4/Exercise4_15.java
@@ -22,31 +22,31 @@ public class Exercise4_15 {
 		//Scannerクラスに変数を設定。
 		Scanner standerdInput = new Scanner(System.in);
 		//身長の最低値と最高値を表す変数に初期値0を代入。
-		int underHight = 0, overHight = 0, intervalVariable = 0;
+		int underHeight = 0, overHeight = 0, intervalVariable = 0;
 		//入力された整数値が0以下の場合繰り返し
-		for (; underHight <= 0;) {
+		for (; underHeight <= 0;) {
 			//入力を促す。
 			System.out.print("何cmから：");
 			//入力を読み込む。
-			underHight = standerdInput.nextInt();
+			underHeight = standerdInput.nextInt();
 			//入力値が0以下の場合
-			if (underHight <= 0) {
+			if (underHeight <= 0) {
 				//正の整数値の入力を促す文章を表示。
 				System.out.print("正しく入力してください。");
 			}
 		}
 		//入力された整数値が0以下、または先に入力された値より小さい場合繰り返し
-		for (; overHight <= 0 || overHight <= underHight;) {
+		for (; overHeight <= 0 || overHeight <= underHeight;) {
 			//入力を促す。
 			System.out.print("何cmまで：");
 			//入力を読み込む。
-			overHight = standerdInput.nextInt();
+			overHeight = standerdInput.nextInt();
 			//整数値が0以下の場合
-			if (overHight <= 0) {
+			if (overHeight <= 0) {
 				//正の整数値の入力を促す文章を表示。
 				System.out.print("正しく入力してください。");
 			} //後から入力した整数値が先に入力した整数値より小さい場合。
-			if (overHight <= underHight) {
+			if (overHeight <= underHeight) {
 				//先に入力した身長より高い身長の入力を促す文章を表示。
 				System.out.print("先に入力した身長より高い身長を入力してください。");
 			}
@@ -70,20 +70,20 @@ public class Exercise4_15 {
 		//計算に使用する定数に0.9を設定。
 		final double POINT_NINE = 0.9;
 		//入力された身長の最低値から最高値までの標準体重を計算
-		for (; underHight <= overHight;) {
+		for (; underHeight <= overHeight;) {
 			//実数値の変数に平均体重を計算して代入
-			double perWeight = (underHight - CONSTANT_HUNDRED) * POINT_NINE;
+			double perWeight = (underHeight - CONSTANT_HUNDRED) * POINT_NINE;
 			//平均体重に小数点以下の数字が発生する場合
 			if (perWeight * CONSTANT_TEN % CONSTANT_TEN != 0) {
 				//身長と標準体重を表示。
-				System.out.println(underHight + "　 " + perWeight);
+				System.out.println(underHeight + "　 " + perWeight);
 				//平均体重に小数点以下の数字が発生しない場合
 			} else {
 				//定数を用いて身長と標準体重を計算して表示。
-				System.out.println(underHight + "　 " 
-				+ (underHight - CONSTANT_HUNDRED) * CONSTANT_NINE / CONSTANT_TEN);
+				System.out.println(underHeight + "　 " 
+				+ (underHeight - CONSTANT_HUNDRED) * CONSTANT_NINE / CONSTANT_TEN);
 			} //身長に表示する幅の値を足して次のループへ。
-			underHight = underHight + intervalVariable;
+			underHeight = underHeight + intervalVariable;
 		}
 
 	}
